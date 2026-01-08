c170798eecea08bfa2f94f13929c89626f1eea86
HariBhandari-02 <bhandarryharry@gmail.com>
2026-01-08 21:58:01 +0545

    Router,controller & model done

--- backend/src/controller/postUser.ts
+++ backend/src/controller/postUser.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express";
+import { postUser } from "../model/userModel";
 
 export type UserPostInput = {
   title: string;
   image: string;
 };
 
-export async function postUser(req: Request, res: Response) {
+export async function postUserController(req: Request, res: Response) {
   const body = req.body as UserPostInput;
 
   const postedUser = await postUser(body);
--- backend/src/router/user-router.ts
+++ backend/src/router/user-router.ts
@@ -1,9 +1,11 @@
 import { Application } from "express";
+import { postUserController } from "../controller/postUser";
+import { getUserController } from "../controller/getuser";
 
 export async function UserRouter(app: Application) {
   //post users data
-  app.post("/users/post");
+  app.post("/users/post", postUserController);
 
   //get user data
-  app.get("/users/get");
+  app.get("/users/get", getUserController);
 }
